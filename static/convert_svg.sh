c9aa0484425f6f04ccbbdc1e78628e0930cfe934
Hamid Ebadi <hamid.ebadi@gmail.com>
2023-10-27 21:48:21 +0200

    update web

--- static/convert_svg.sh
+++ static/convert_svg.sh
@@ -1,21 +1,20 @@
 
 # uncoment for re generating these images
-#convert -density 200 logo.svg -resize 57x57 -transparent white logo57x57.png
-#convert -density 200 logo.svg -resize 100x100 -transparent white logo100x100.png
-#convert -density 200 logo.svg -resize 200x200 -transparent white logo200x200.png
-#convert -density 200 logo.svg -resize 180x180 -transparent white logo180x180.png
-#convert -density 200 logo.svg -resize 350x350 -transparent white logo350x350.png
-#convert -density 200 logo.svg -resize 512x512 -transparent white logo512x512.png
+convert -background none   logo.svg -resize 57x57  logo57x57.png
+convert -background none   logo.svg -resize 100x100  logo100x100.png
+convert -background none   logo.svg -resize 200x200  logo200x200.png
+convert -background none   logo.svg -resize 180x180  logo180x180.png
+convert -background none   logo.svg -resize 350x350  logo350x350.png
+convert -background none   logo.svg -resize 512x512  logo512x512.png
 
-#convert -density 200 logo-with-circle.svg -resize 57x57 -transparent white logo-with-circle57x57.png
-#convert -density 200 logo-with-circle.svg -resize 100x100 -transparent white logo-with-circle100x100.png
-#convert -density 200 logo-with-circle.svg -resize 200x200 -transparent white logo-with-circle200x200.png
-#convert -density 200 logo-with-circle.svg -resize 180x180 -transparent white logo-with-circle180x180.png
-#convert -density 200 logo-with-circle.svg -resize 350x350 -transparent white logo-with-circle350x350.png
-#convert -density 200 logo-with-circle.svg -resize 512x512 -transparent white logo-with-circle512x512.png
+convert -background none   logo-with-circle.svg -resize 57x57  logo-with-circle57x57.png
+convert -background none   logo-with-circle.svg -resize 100x100  logo-with-circle100x100.png
+convert -background none   logo-with-circle.svg -resize 200x200  logo-with-circle200x200.png
+convert -background none   logo-with-circle.svg -resize 180x180  logo-with-circle180x180.png
+convert -background none   logo-with-circle.svg -resize 350x350  logo-with-circle350x350.png
+convert -background none   logo-with-circle.svg -resize 512x512  logo-with-circle512x512.png
 
-
-convert -density 256x256 -background transparent logo.svg -define icon:auto-resize -colors 256 favicon.ico
+convert -background none -density 256x256 -background transparent logo.svg -define icon:auto-resize -colors 256 favicon.ico
 rm ../favicon.ico
 cp favicon.ico ../favicon.ico
 
@@ -26,17 +25,18 @@ cp favicon.ico ../favicon.ico
 #├── icon-background.png
 #├── splash.png
 #└── splash-dark.png
-#    Icon files should be at least 1024px x 1024px.
-#    Splash screen files should be at least 2732px x 2732px.
-#    The format can be jpg or png.
+#   Icon files should be at least 1024px x 1024px.
+#   Splash screen files should be at least 2732px x 2732px.
+#   The format can be jpg or png.
 
 
-convert -density 288 -background none  logo-with-circle.svg -gravity center -resize 1024x1024 -extent 1200x1200 -alpha off -opaque black -alpha on  icon-only.png
-convert -density 288 -background none  logo-with-circle.svg -gravity center -resize 1024x1024 -extent 1200x1200 -alpha off -opaque black -alpha on  icon-foreground.png
-convert -density 288 -background none  logo-with-circle.svg -gravity center -resize 1024x1024 -extent 1200x1200 -alpha off -opaque black -alpha on  icon-background.png
-convert -density 288 -background white logo-with-circle.svg -gravity center -resize 500x500 -extent 3000x3000 -alpha off -opaque red -alpha off  splash.png
-convert -density 288 -background white logo-with-circle.svg -gravity center -resize 500x500 -extent 3000x3000 -alpha off -opaque red -alpha off  splash-dark.png
+convert -background none -density 288  logo-with-circle.svg -gravity center -resize 1024x1024 icon-only.png
+convert -background none -density 288  logo-with-circle.svg -gravity center -resize 1024x1024 icon-foreground.png
+convert -background none -density 288  logo-with-circle.svg -gravity center -resize 1024x1024 icon-background.png
+convert -background none -density 288  logo-with-circle.svg -gravity center -resize 500x500   splash.png
+convert -background none -density 288  logo-with-circle.svg -gravity center -resize 500x500   splash-dark.png
 
+png2icns favicon.icns icon-only.png
 
 
 
